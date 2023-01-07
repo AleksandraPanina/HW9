a7e2cf1e1b9376d4b4ce82c5e1f16bb9c8a7e73e
Aleksandra <canonfine90@mail.ru>
2023-01-07 17:35:45 +0300

    added task1

--- Program.cs
+++ Program.cs
@@ -22,3 +22,32 @@ void GapNumberSum (int numberM, int numberN, int sum)
 
 GapNumberSum(numberM, numberN, 0);
 
+
+
+
+//Задача 64: Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа 
+//в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+Console.WriteLine("Input natural number > 1:");
+int number = int.Parse(Console.ReadLine());
+
+///Метод вывода натуральных чисел от N до 1:
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} not natural number");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
+
+
+
+
